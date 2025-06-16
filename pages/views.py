058e11fe2f5c74b78e99804adde17b139e6eced5
kevin <kvzhang2002@gmail.com>
2025-06-16 16:57:15 -0400

    create and link some more basic page templates

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'pages/homepage.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'profiles'
+
 urlpatterns = [
     path('', views.ProfileListView.as_view(), name='profile-list'),
     path('<str:name>/', views.ProfileDetailView.as_view(), name='profile-detail'),
