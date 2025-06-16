331dda7bacc8b7ae5d2f7211fd381f728245696e
kevin <kvzhang2002@gmail.com>
2025-06-16 02:16:44 -0400

    initialize profiles app

---
+++ profiles/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class CreativeField(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    # pfp
+    bio = models.TextField()
+    creative_fields = models.ManyToManyField(CreativeField)
+
+    def __str__(self):
+        return self.name
+
+
+class PortfolioLink(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='portfolio_links')
+    link = models.URLField()
+
+    def __str__(self):
+        return self.link
