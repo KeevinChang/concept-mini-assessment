30e1f70231aebd8a06797fb520a772478d98e75d
kevin <kvzhang2002@gmail.com>
2025-06-16 02:51:48 -0400

    tweak profile urls and add to main project urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-from .views import ProfileListView, ProfileDetailView
 
-path('profiles/', ProfileListView.as_view(), name='profile-list')
-path('profiles/<str:name>', ProfileDetailView.as_view(), name='profile-detail')
+urlpatterns = [
+    path('', views.ProfileListView.as_view(), name='profile-list'),
+    path('<str:name>/', views.ProfileDetailView.as_view(), name='profile-detail'),
+]
