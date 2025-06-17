0c61acff0af120060e30864965fcd77142611311
kevin <kvzhang2002@gmail.com>
2025-06-17 01:36:42 -0400

    add basic listing and display of profiles

--- profiles/views.py
+++ profiles/views.py
@@ -26,7 +26,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileForm
     template_name = 'profiles/profile_create.html'
-    success_url = reverse_lazy('profiles:profile_list')
+    success_url = reverse_lazy('profiles:profile-list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
