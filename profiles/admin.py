a13ce0fd7542ed21b0997353452152502d9d3471
kevin <kvzhang2002@gmail.com>
2025-06-17 00:11:32 -0400

    add models to django admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, CreativeField, PortfolioLink
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(CreativeField)
+admin.site.register(PortfolioLink)
