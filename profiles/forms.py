24b4713126734f928857184ab56af15daf0ed67e
kevin <kvzhang2002@gmail.com>
2025-06-16 23:49:29 -0400

    basic profile creation page added

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,3 +1,20 @@
 from django import forms
-from .models import Profile
+from django.forms import inlineformset_factory
 
+from .models import Profile, PortfolioLink
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'bio', 'creative_fields']
+        widgets = {
+            'creative_fields': forms.CheckboxSelectMultiple(),
+        }
+
+PortfolioLinkFormSet = inlineformset_factory(
+    Profile, PortfolioLink,
+    fields=['link'],
+    extra=3,
+    max_num=3,
+    can_delete=True
+)
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Profile, CreativeField, PortfolioLink
+from .forms import ProfileForm, PortfolioLinkFormSet
 
 # Create your views here.
 
@@ -18,3 +20,29 @@ class ProfileListView(ListView):
 
 class ProfileDetailView(DetailView):
     model = Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'profiles/profile_create.html'
+    success_url = reverse_lazy('profiles:profile_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PortfolioLinkFormSet(self.request.POST)
+        else:
+            context['formset'] = PortfolioLinkFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.success_url)
+        else:
+            return self.form_invalid(form)
