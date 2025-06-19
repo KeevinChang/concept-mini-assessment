c1b63b5db56dea90a6b338ef9a4e6317691d8934
kevin <kvzhang2002@gmail.com>
2025-06-18 22:29:21 -0400

    clean up changed/deleted profile pictures

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+import os
 # Create your models here.
 
 class CreativeField(models.Model):
@@ -17,6 +19,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        try:
+            old = Profile.objects.get(pk=self.pk)
+            default_image = self._meta.get_field('profile_picture').default
+            if old.profile_picture != self.profile_picture and old.profile_picture.name != default_image:
+                if os.path.isfile(old.profile_picture.path):
+                    os.remove(old.profile_picture.path)
+        except Profile.DoesNotExist:
+            pass
+        super().save(*args, **kwargs)
+
+
+@receiver(post_delete, sender=Profile)
+def delete_profile_picture(sender, instance, **kwargs):
+    default_image = instance._meta.get_field('profile_picture').default
+    if instance.profile_picture.name != default_image and instance.profile_picture:
+        file_path = instance.profile_picture.path
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 
 class PortfolioLink(models.Model):
